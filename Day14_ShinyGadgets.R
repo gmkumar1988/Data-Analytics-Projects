54075e293b6b7fc1ec49e774b81de29a270a3911
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-05-16 22:39:41 -0400

    Day14 Shiny Gadgets

---
+++ Day14_ShinyGadgets.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(miniUI)
+
+multiplynumbers = function(number1, number2) {
+  ui <- miniPage(
+    gadgetTitleBar("Multiply Two numbers"),
+    miniContentPanel(
+      selectInput("num1","First Number",choices = number1),
+      selectInput("num2","Second Number",choices = number2)
+    )
+    
+  )
+  server <- function(input,output, session) {
+    observeEvent(input$done,{
+      num1 <- as.numeric(input$num1)
+      num2 <- as.numeric(input$num2)
+      stopApp(num1 * num2)
+    })
+  }
+  runGadget(ui,server)
+}
