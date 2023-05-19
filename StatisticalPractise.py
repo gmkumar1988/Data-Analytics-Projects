360dad3721a7887b6689b8b873d33be3442f9dfc
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-05-19 19:06:23 -0400

    Pythonstatistics1

---
+++ StatisticalPractise.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 18:54:06 2023
+
+@author: maheshg
+"""
+
+import numpy as np
+
+a = np.array([1,2,3])
+
+print(type(a))
+
+print(np.shape(a))
+
+b = np.array([[1,2],[3,4]])
+
+print(type(b))
+
+print(np.shape(b))
+
+print(b)
+
+b[1,0]
+
+c = np.zeros((3,2))
+
+print(c)
+
+d = np.ones((3,2))
+
+print(d)
+
+e = np.full((3,3),9)
+
+print(e)
+
+f = np.random.random((3,3))
+
+print(f)
