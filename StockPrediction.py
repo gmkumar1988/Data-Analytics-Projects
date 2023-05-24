34fdcd0fc70bd5af5fb0cb90aaaa19e54df8edf4
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-05-24 19:10:51 -0400

    Stock Prediction

---
+++ StockPrediction.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 18:40:36 2023
+
+@author: maheshg
+"""
+"Recurring Neural Networks "
+
+from pathlib import *
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import yfinance as yf
+import pandas_ta as ta
