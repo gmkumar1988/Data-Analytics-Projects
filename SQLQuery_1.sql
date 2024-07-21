17586f2e8179a3af61c770d2e6f8c664b16f1bfe
MaheshG <48827109+gmkumar1988@users.noreply.github.com>
2024-07-21 10:39:12 -0400

    Daily Practise

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,25 +1,31 @@
--- SELECT 
---     [Buying Group],
---     COUNT([Customer Key]) as custcount
--- FROM dimCustomer
--- GROUP BY [Buying Group]
--- ORDER BY custcount
+-- -- SELECT 
+-- --     [Buying Group],
+-- --     COUNT([Customer Key]) as custcount
+-- -- FROM dimCustomer
+-- -- GROUP BY [Buying Group]
+-- -- ORDER BY custcount
 
 
+-- -- SELECT 
+-- --     [Postal Code]
+-- -- FROM dimCustomer
+-- -- WHERE [Buying Group] = 'Wingtip Toys'
+-- -- GROUP BY [Postal Code]
+-- -- HAVING COUNT([Customer Key]) > 3
+
 -- SELECT 
---     [Postal Code]
+--     [Postal Code],
+--     [Customer]
 -- FROM dimCustomer
--- WHERE [Buying Group] = 'Wingtip Toys'
--- GROUP BY [Postal Code]
--- HAVING COUNT([Customer Key]) > 3
+-- WHERE [Postal Code] IN (SELECT
+--                             [Postal Code]
+--                             FROM dimCustomer
+--                             where [Buying Group]= 'Wingtip Toys'
+--                             GROUP BY [Postal Code]
+--                             HAVING COUNT ([Customer Key])> 3)
 
 SELECT 
-    [Postal Code],
-    [Customer]
-FROM dimCustomer
-WHERE [Postal Code] IN (SELECT
-                            [Postal Code]
-                            FROM dimCustomer
-                            where [Buying Group]= 'Wingtip Toys'
-                            GROUP BY [Postal Code]
-                            HAVING COUNT ([Customer Key])> 3)
+    COUNT(Employee) as EmplCount
+    FROM dimEmployee
+WHERE Employee != 'Unknown'
+
